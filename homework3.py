2855751c8a3e3ff68df556b88e0cb1b8e530e31c
Laura Bostan <sarnthil@gmail.com>
2015-12-16 23:27:06 +0100

    added the hw

---
+++ homework3.py
@@ -0,0 +1,18 @@
+'''You've been handed an encrypted file!
+Luckily, you know it's a monoalphabetic
+substitution cipher. And thankfully, you
+have got a reference corpus of the same
+language handy. Finally, you feel so
+confident in the sizes of the texts, that
+you feel the distributions might just match.
+
+You may assume anything other than the letters
+from a-z is unencrypted.'''
+
+def goldbug(encfile="brown-sample-enc.txt", reffile="brown.txt"):
+    '''Reads an encrypted file and a reference file and prints
+    the decrypted text using frequency analysis. Have fun!'''
+    pass
+
+if __name__ == '__main__':
+    goldbug()
